08ed5e278eb30ac01a776eb99b856ef3504ab754
Szőcs Áron <szocs.aa@gmail.com>
2025-08-07 18:28:26 +0200

    init

---
+++ public/client.js
@@ -0,0 +1,40 @@
+const socket = new WebSocket(`ws://${location.host}`);
+const startBtn = document.getElementById("start");
+const stopBtn = document.getElementById("stop");
+const bpmInput = document.getElementById("bpm");
+
+let audioCtx = new (window.AudioContext || window.webkitAudioContext)();
+let isRunning = false;
+
+function playClick() {
+  const osc = audioCtx.createOscillator();
+  const gain = audioCtx.createGain();
+  osc.type = "square";
+  osc.frequency.setValueAtTime(1000, audioCtx.currentTime);
+  gain.gain.setValueAtTime(1, audioCtx.currentTime);
+  osc.connect(gain).connect(audioCtx.destination);
+  osc.start();
+  osc.stop(audioCtx.currentTime + 0.05);
+}
+
+socket.onmessage = (msg) => {
+  const data = JSON.parse(msg.data);
+
+  if (data.type === "tick") {
+    playClick();
+  } else if (data.type === "start") {
+    isRunning = true;
+    bpmInput.value = data.bpm;
+  } else if (data.type === "stop") {
+    isRunning = false;
+  }
+};
+
+startBtn.onclick = () => {
+  const bpm = parseInt(bpmInput.value, 10);
+  socket.send(JSON.stringify({ type: "start", bpm }));
+};
+
+stopBtn.onclick = () => {
+  socket.send(JSON.stringify({ type: "stop" }));
+};
---
+++ server.js
@@ -0,0 +1,61 @@
+const express = require("express");
+const http = require("http");
+const WebSocket = require("ws");
+
+const app = express();
+const server = http.createServer(app);
+const wss = new WebSocket.Server({ server });
+
+const PORT = 3000;
+let interval = null;
+let bpm = 120;
+
+app.use(express.static("public"));
+
+function broadcast(data) {
+  wss.clients.forEach((client) => {
+    if (client.readyState === WebSocket.OPEN) {
+      client.send(JSON.stringify(data));
+    }
+  });
+}
+
+function startMetronome() {
+  const beatInterval = (60 / bpm) * 1000;
+  broadcast({ type: "start", bpm });
+
+  interval = setInterval(() => {
+    broadcast({ type: "tick", timestamp: Date.now() });
+  }, beatInterval);
+}
+
+function stopMetronome() {
+  clearInterval(interval);
+  interval = null;
+  broadcast({ type: "stop" });
+}
+
+wss.on("connection", (ws) => {
+  console.log("Client connected");
+
+  ws.send(JSON.stringify({ type: interval ? "start" : "stop", bpm }));
+
+  ws.on("message", (msg) => {
+    const data = JSON.parse(msg);
+
+    if (data.type === "start") {
+      bpm = data.bpm || 120;
+      if (!interval) startMetronome();
+    } else if (data.type === "stop") {
+      stopMetronome();
+    }
+  });
+
+  ws.on("close", () => {
+    console.log("Client disconnected");
+  });
+});
+
+server.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`);
+});
